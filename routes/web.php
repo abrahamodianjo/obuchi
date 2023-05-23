2daf77c79389d0da78b5b11325cf3652c6513a1e
abrahamodianjo <odianjoabraham@outlook.com>
2023-05-23 16:53:25 +0100

    designed admin panel part 1

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
