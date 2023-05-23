a7b8e00de5c0c78e3494a0ab0e2a186393e88bae
abrahamodianjo <odianjoabraham@outlook.com>
2023-05-23 20:10:38 +0100

    fixed the logout button and created a redirect to the login page #1

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,31 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Demo\DemoController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'Index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'ContactMethod')->name('contact.page');
+});
+
+
+//Admin All Route
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+   
+});
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('admin.index');
