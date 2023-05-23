112e1f69ff519f4c8ddaeb7c8816aaf69cf3ee6f
abrahamodianjo <odianjoabraham@outlook.com>
2023-05-23 18:26:04 +0100

    designed admin panel part 3 (completed)

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,11 +2,11 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                <script>document.write(new Date().getFullYear())</script> © Upcube.
+                               O'Buchi Multimedia Ltd © Upcube.
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Abraham Odianjo
                                 </div>
                             </div>
                         </div>
