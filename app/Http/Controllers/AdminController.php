dde4e0a79b0a79f480094322392b626631655551
abrahamodianjo <odianjoabraham@outlook.com>
2023-05-24 16:09:37 +0100

    designed the profile page #5

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -18,4 +19,13 @@ class AdminController extends Controller
 
         return redirect('/login');
     } //End Method
+
+
+
+    public function Profile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id); 
+        return view('admin.admin_profile_view', compact('adminData'));
+
+    }//End Method
 }
---
+++ resources/views/admin/admin_profile_view.blade.php
@@ -0,0 +1,34 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<div class="page-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="card">
+                   
+                    <center> <br><br>
+                    <img class="rounded-circle avatar-xl" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                    </center>
+
+                   
+
+                        <div class="card-body">
+                            <h4 class="card-title">Name: {{$adminData->name}}</h4>
+                            <hr>
+                            <h4 class="card-title">User Email: {{$adminData->email}}</h4>
+                            <hr>
+                            <h4 class="card-title">User Name: {{$adminData->username}}</h4>
+                            <hr>
+                            <a href="" class="btn btn-danger btn-rounded waves-effect waves-light"> Edit Profile</a>
+                                
+                                
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
