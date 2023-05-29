3d6463637c9e73dc3bad2d294cf11c05ce727213
abrahamodianjo <odianjoabraham@outlook.com>
2023-05-29 16:00:39 +0100

    successfully designed the home slider section #10

--- resources/views/admin/home_slide/home_slide_all.blade.php
+++ resources/views/admin/home_slide/home_slide_all.blade.php
@@ -54,11 +54,11 @@
               <div class="row mb-3">
                  <label for="example-text-input" class="col-sm-2 col-form-label">  </label>
                 <div class="col-sm-10">
-  <img id="showImage" class="rounded avatar-lg" src="{{ (!empty($homeslide->home_slide))? url($homeslide->home_slide):url('upload/no_image.jpg') }}" alt="Card image cap">
+                    <img id="showImage" class="rounded avatar-lg" src="{{ (!empty($homeslide->home_slide))? url($homeslide->home_slide):url('upload/no_image.jpg') }}" alt="Card image cap">
                 </div>
             </div>
             <!-- end row -->
-<input type="submit" class="btn btn-danger waves-effect waves-light" value="Update Slide">
+        <input type="submit" class="btn btn-danger waves-effect waves-light" value="Update Slide">
             </form>
              
            
