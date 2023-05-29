f9c9189d10bc6a924962284da6fa8c01be549ac0
abrahamodianjo <odianjoabraham@outlook.com>
2023-05-29 13:24:34 +0100

    updated the home slider section to connect with the database then trigger the upload image to display it when editing #10

--- resources/views/admin/home_slide/home_slide_all.blade.php
+++ resources/views/admin/home_slide/home_slide_all.blade.php
@@ -54,7 +54,7 @@
               <div class="row mb-3">
                  <label for="example-text-input" class="col-sm-2 col-form-label">  </label>
                 <div class="col-sm-10">
-  <img id="showImage" class="rounded avatar-lg" src="{{ (!empty($homeslide->home_slide))? url( $homeslide->home_slide):url('upload/no_image.jpg') }}" alt="Card image cap">
+  <img id="showImage" class="rounded avatar-lg" src="{{ (!empty($homeslide->home_slide))? url($homeslide->home_slide):url('upload/no_image.jpg') }}" alt="Card image cap">
                 </div>
             </div>
             <!-- end row -->
--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::controller(AdminController::class)->group(function () {
 //Home Slide All Route
 Route::controller(HomeSliderController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
-    Route::post('/home/update', [HomeSliderController::class, 'update'])->name('update.slider');
+    Route::post('/update/slider','UpdateSlider' )->name('update.slider');
    
    
 });
