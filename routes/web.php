7f67ef10178a84596f85783b9e69eb2723eb81c9
abrahamodianjo <odianjoabraham@outlook.com>
2023-06-11 13:11:28 +0100

    implemented the middleware for admin login functionallity

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,21 @@ Route::controller(DemoController::class)->group(function () {
 
 
 //Admin All Route
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/admin/logout', 'destroy')->name('admin.logout');
-    Route::get('/admin/profile', 'Profile')->name('admin.profile');
-    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
-    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+Route::middleware(['auth'])->group(function () {
+ 
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/admin/logout', 'destroy')->name('admin.logout');
+        Route::get('/admin/profile', 'Profile')->name('admin.profile');
+        Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+        Route::post('/store/profile', 'StoreProfile')->name('store.profile');
 
-    Route::get('/change/password', 'ChangePassword')->name('change.password');
-    Route::post('/update/password', 'Updatepassword')->name('update.password');
-   
+        Route::get('/change/password', 'ChangePassword')->name('change.password');
+
+        Route::get('/change/password', 'ChangePassword')->name('change.password');
+        Route::post('/update/password', 'Updatepassword')->name('update.password');
+    
+    });
+
 });
 
 //Home Slide All Route
