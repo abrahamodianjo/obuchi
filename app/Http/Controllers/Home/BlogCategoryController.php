f6e755b0f187f77bc9fbf9b5abd57b1de68bebda
abrahamodianjo <odianjoabraham@outlook.com>
2023-06-11 13:44:51 +0100

    implemented the form validation using javascript

--- app/Http/Controllers/Home/BlogCategoryController.php
+++ app/Http/Controllers/Home/BlogCategoryController.php
@@ -22,16 +22,6 @@ class BlogCategoryController extends Controller
     }// End add blog category method
 
     public function StoreBlogCategory(Request $request){
-
-        $request->validate([
-            'blog_category' => 'required',
-           
-        ], [
-
-            'blog_category.required' => 'Blog Category Name is Required',
-           
-        ]);
-
         
         BlogCategory::insert([
                 'blog_category' => $request->blog_category,
